01de653731fc1b23538b2ae961c29b5ce6d5018c
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-06-23 13:10:36 -0400

    Added maths
    x = -1
    math.sqrt( x )

---
+++ Random/Math/Math1.py
@@ -0,0 +1,16 @@
+# this is some math stuffs
+
+#from __future__ import division
+
+# division
+print 5/2
+print 5//2
+
+# These answers are the same - they should be different - why is that? ... Further research is needed
+# the first is Floating point division, and the other is floor division, sometimes also called integer division.
+
+# Let's try something else
+#from __future__ import division
+print float(5)/2
+
+#now we get the correct value for the maths unless we want to import division
