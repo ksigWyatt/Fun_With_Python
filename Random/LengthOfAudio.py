afa67ec116014a000411a03de52b9789cccf757a
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-06-28 08:18:18 -0400

    Added tinyag to LengthOfAudio
    I found quite an interesting conundrum, Wav files are often used to hold important metadata that otherwise often gets lost with other file formats when the files have been modified. The artist tag is used to store important JSON Metadata in this example. However, Tiny tag can't read my wav unless its mp3. In the properties it displays the data for wav, and not so for mp3. Interesting.

--- Random/LengthOfAudio.py
+++ Random/LengthOfAudio.py
@@ -1,20 +1,44 @@
-import wave
-import contextlib
+# import wave, contextlib
+from tinytag import TinyTag
+
 
 # assign the file to the audio source
-audio = 'C:\Users\wyatt\Desktop\chapter.wav'
+#audio = 'C:/Users/wyatt/Downloads/chapter.mp3'
+audio = 'C:/Users/wyatt/Downloads/chapter.mp3'
 
-#read the file
-with contextlib.closing(wave.open(audio, 'r')) as f:
-    #get the number of frames in the file
-    frames = f.getnframes()
-    #get the frame rate
-    rate = f.getframerate()
-    #calculate duration
-    duration = frames / float(rate)
-    #seconds of length
-    print duration,'seconds long'
 
-    #calculate minutes
-    minutes = duration / 60
-    print float(minutes)
+tag = TinyTag.get(audio)
+print (float(tag.filesize) / 1000000), "MB"
+print ""
+print tag
+print ""
+print 'It is %f seconds long.' % tag.duration
+print ""
+
+# # read the file
+# with contextlib.closing(wave.open(audio, 'r')) as f:
+#     #get the number of frames in the file
+#     frames = f.getnframes()
+#     #get the frame rate
+#     rate = f.getframerate()
+#     #calculate duration
+#     duration = frames / float(rate)
+#     #seconds of length
+#     print duration,'Seconds long'
+#
+#     #calculate minutes
+#     minutes = duration / 60
+#     print float(minutes), "Minutes long"
+#     print ""
+#     print f.tell()
+#     f.close()
+
+
+
+# wavFile = wave.open(audio)
+# length = wavFile.getnframes()
+
+# for i in range(0, length):
+#     waveData = wavFile.readframes(1)
+#     data = struct.unpack("<h", waveData)
+    # print int(data[0])
