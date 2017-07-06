6c206ad87ed9b1c7d030e9b42130342ead52c86b
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-07-06 14:07:13 -0400

    Archival of converted .wav to .mp3 files - working

--- Random/Creating & Writing/CreateZIP.py
+++ Random/Creating & Writing/CreateZIP.py
@@ -4,7 +4,7 @@ import os
 
 #assuming the directory path is absolute - this will work
 # change this to fit your project
-directory = '/Fun_With_Python/Audio'
+directory = 'C:/Users/wyatt/Desktop/Wyatt/Python_Code/Git/Fun_With_Python/Audio'
 
 # Create an empty array of files in the zip
 filesInZip = []
@@ -12,20 +12,23 @@ filesInZip = []
 # for all files, sub-folders in a directory
 for subdir, dirs, files in os.walk(directory):
     # look at all the files
+    print
     for file in files:
         # store the absolute path which is is it's subdir and where the os step is
         filePath = subdir + os.sep + file
+        print filePath
 
         # if the file is audio
         if filePath.endswith(".wav") or filePath.endswith(".mp3"):
             # Add to array so it can be added to the archive
-            filesInZip.append(filePath.title().lower())
-
-
-# using zip file create a file called zipped_file.zip
-# adding the members ot filesInZip array to the compressed file
-with zipfile.ZipFile('zipped_file.zip', 'w') as zipped_f:
-    # for all the member in the array of files add them to the zip archive
-    # doing this - this way also preserves exactly the directory location that the files sit in even before the main archive
-    for members in filesInZip:
-        zipped_f.write(members)
+            inputFile = filePath.title().lower()
+            print file.title().lower()
+            print inputFile
+#
+# # using zip file create a file called zipped_file.zip
+# # adding the members ot filesInZip array to the compressed file
+# with zipfile.ZipFile('zipped_file.zip', 'w') as zipped_f:
+#     # for all the member in the array of files add them to the zip archive
+#     # doing this - this way also preserves exactly the directory location that the files sit in even before the main archive
+#     for members in filesInZip:
+#         zipped_f.write(members)
--- Random/Stuffs with Audio/WavToMp3.py
+++ Random/Stuffs with Audio/WavToMp3.py
@@ -1,6 +1,5 @@
 from pydub import AudioSegment
 
-# read from file
 inputFile = "chapter.wav"
 
 #convert to mp3
